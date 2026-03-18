0775f0d6e8fee855b0088f6bb1f073c2d1dbfa3e
hdarlim <henriquedarlim@gmail.com>
2026-03-18 19:04:27 -0300

    Refactor input handling to unpack values directly into variables a, b, and c

--- 2nd degree equation solver.py
+++ 2nd degree equation solver.py
@@ -1,3 +1,5 @@
-equation = input("Enter 'a', 'b', and 'c' separated by spaces (ex.: 2 -3 9): ")
+a, b, c = map(int, input("Enter 'a', 'b', and 'c' separated by spaces (ex.: 2 -3 9): ").split())
 # test
-print(equation)
+print(a)
+print(b)
+print(c)
